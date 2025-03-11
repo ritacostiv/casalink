5330a5536e5bf32decb6e4707ad97d15f47afc13
Simao Martins <simaopmartins@gmail.com>
2025-03-11 17:55:01 +0000

    collection popup with collapsable

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,16 +1,15 @@
 class CollectionsController < ApplicationController
   def index
     @collections = Collection.all
-  end
-
-  def new
+    @collection = Collection.new
   end
 
   def create
     @collection = Collection.new(collection_params)
+    @collection.user = current_user
 
     if @collection.save
-      redirect_to collections_path, notice: "success"
+      redirect_to collection_path(@collection), notice: "success"
     else
       render :new, status: :unprocessable_entity
     end
@@ -19,7 +18,7 @@ class CollectionsController < ApplicationController
   def show
     @collection = Collection.find(params[:id])
   end
-  
+
   private
 
   def collection_params
