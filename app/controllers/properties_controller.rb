a2f98f2f66e3e8964159047f777083233dab0f2d
Simao Martins <simaopmartins@gmail.com>
2025-03-12 15:38:22 +0000

    property creation done

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,19 @@
 class PropertiesController < ApplicationController
-  
+  def create
+    @collection = Collection.find(params[:collection_id])
+    @property = Property.new(property_params)
+    @property.collection = @collection
+
+    if @property.save
+      redirect_to collection_path(@collection), notice: "success"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :url)
+  end
 end
