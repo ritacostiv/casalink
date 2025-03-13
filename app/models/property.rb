defe50ed6fa8a4e22dd0201423005624741cbf49
Simao Martins <simaopmartins@gmail.com>
2025-03-13 18:08:10 +0000

    map refactored

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,12 @@
 class Property < ApplicationRecord
   belongs_to :collection
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   # after_create :scrape_property_details
 
   # private
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 
   #private
 
