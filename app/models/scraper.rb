0067a2e8e6abc4321347bbb052fc7d42e4c99985
Rita Costiv <ritacostiv3@gmail.com>
2025-03-13 14:07:56 +0000

    added scraper model

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -1,2 +1,19 @@
-class Scraper < ApplicationRecord
+require 'open-uri'
+require 'nokogiri'
+
+class Scraper
+  def scrape_data(url)
+      # Fetch and parse HTML
+      doc = Nokogiri::HTML(URI.open(url))
+
+      # Example: Extract title from the webpage
+      property_name = doc.css('.main-info__title-main').text.strip
+
+
+      # Save extracted data (customize based on your needs)
+      update(
+        title: title,
+        description: description
+      )
+  end
 end
