8e04216a822b0a9e8b3ed2716ba8833fb64d3187
Simao Martins <simaopmartins@gmail.com>
2025-03-14 16:04:06 +0000

    fixed image import and map markers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_13_154352) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_14_144217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_154352) do
     t.bigint "collection_id", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "image1"
+    t.string "image2"
+    t.string "image3"
+    t.string "image4"
     t.index ["collection_id"], name: "index_properties_on_collection_id"
   end
 
