7fcac2e3540964a93b3656a9fe36c4265b9e0621
miguel rodrigues <mike_slx@yahoo.com>
2025-03-17 12:16:29 +0000

    mycollection icon added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,5 +5,5 @@ import "@popperjs/core"
 import "bootstrap"
 
 import './background_slider';
-import '../components/animate_feature_row';
+import './components/animate_feature_row';
 import './components/popup_card';
