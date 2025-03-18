99c0081b1399bb7c37f2a4443e5c2032346abe42
Simao Martins <simaopmartins@gmail.com>
2025-03-18 15:40:01 +0000

    comments added to recent activity

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,10 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.property = @property
     @comment.user = current_user
+    @collection = @property.collection
 
     if @comment.save
+      create_event(:comment_creation)
       redirect_to collection_path(@property.collection), notice: "Comment Added"
     else
       redirect_to collection_path(@property.collection), alert: "Failed to add comment"
@@ -16,5 +18,21 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text)
+  end
+
+  def create_event(event_type)
+    Event.create(
+      user_id: current_user.id,
+      property: @property,
+      comment_text: @comment.text,
+      comment_id: @comment.id,
+      user_first_name: current_user.first_name,
+      user_last_name: current_user.last_name,
+      property_name: @property.name,
+      url: @property.url,
+      collection_name: @collection.name,
+      collection_url: "/collections/#{@collection.id}",
+      event_type: event_type
+    )
   end
 end
