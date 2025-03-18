78127d7f042f4e80bfd26706af663742022472a1
Rita Costiv <ritacostiv3@gmail.com>
2025-03-18 16:41:34 +0000

    added js and scss to show the comment when hovering over it in recent activity

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
+// app/javascript/application.js
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
@@ -6,3 +8,18 @@ import "bootstrap"
 
 import './components/animate_feature_row';
 import './components/popup_card';
+
+
+document.addEventListener('turbo:load', function() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl, {
+      template: '<div class="tooltip custom-tooltip-theme" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner large-tooltip"></div></div>',
+      // Other options:
+      html: true,               // Allow HTML in tooltip content
+      animation: true,          // Fade animation
+      delay: { show: 50, hide: 100 },  // Delay showing/hiding
+      placement: 'top'          // Position (top, bottom, left, right)
+    })
+  })
+})
